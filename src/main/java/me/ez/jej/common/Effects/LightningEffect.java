e7b07580a2654e8eec0f5b438ed05bbdd5166d75
GAMER'S VALLEY <eesaard@gmail.com>
2022-05-25 23:47:49 +0530

    LightningEffect Updated

--- src/main/java/me/ez/jej/common/Effects/LightningEffect.java
+++ src/main/java/me/ez/jej/common/Effects/LightningEffect.java
@@ -1,4 +1,22 @@
 package me.ez.jej.common.Effects;
 
-public class LightningEffect {
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
+
+public class LightningEffect extends MobEffect {
+
+    protected LightningEffect(MobEffectCategory category, int i) {
+        super(category, i);
+    }
+
+    @Override
+    public void applyEffectTick(LivingEntity entity, int amp) {
+        super.applyEffectTick(entity, amp);
+    }
+
+    @Override
+    public boolean isDurationEffectTick(int duration, int amp) {
+        return duration % 5 == 0;
+    }
 }
